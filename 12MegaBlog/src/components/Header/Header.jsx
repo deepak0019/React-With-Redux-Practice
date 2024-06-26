d2e61b4a0e789e3826cee0b258b6ab79bcd0863a
deepak0019 <dangideepak0018@gmail.com>
2024-06-27 01:05:01 +0530

    UI Updation and final commit

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
   ];
 
   return (
-    <header className="py-3 shadow bg-gray-500">
+    <header className="py-3 shadow bg-gray-900">
       <Container>
         <nav className="flex">
           <div className="mr-4">
@@ -51,7 +51,7 @@ function Header() {
                 <li key={item.name}>
                   <button
                     onClick={() => navigate(item.slug)}
-                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    className="inline-bock px-6 py-2 duration-200 text-white hover:bg-blue-100 hover:text-gray-900 rounded-full"
                   >
                     {item.name}
                   </button>
--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
+  return (
+    <div className="flex">
+      <img
+        src="./images/logo2.svg"
+        alt=""
+        srcset=""
+        width="60px"
+        height="50px"
+      />
+      <span className="mt-5 mx-5 text-lg font-semibold text-white">
+        BlogNest
+      </span>
+    </div>
+  );
 }
 
 export default Logo;
